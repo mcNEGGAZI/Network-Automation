eb31b168427fe4a119687d949c86f259ecff5002
NEGGAZI Mohamed Chakib <121987358+mcNEGGAZI@users.noreply.github.com>
2024-04-01 19:26:56 +0100

    update

--- Script automation/infoNAPALM.py
+++ Script automation/infoNAPALM.py
@@ -1,7 +1,7 @@
 from napalm_connection import napalm_connection,napalm_close
 
 def sw():
-    device=napalm_connection('192.168.10.1','chakibng','chakibng','netauto','ios')
+    device=napalm_connection('192.168.10.254','chakibng','chakibng','netauto','ios')
     print(device.get_facts()['interface_list'])
     napalm_close(device)
 
