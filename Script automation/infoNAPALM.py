50d391f71fa23d6a80338d61e10bb3b63b0a59f1
NEGGAZI Mohamed Chakib <mohamedchakib.neggazi@etu.usthb.dz>
2024-04-20 11:55:26 +0100

    update

--- Script automation/infoNAPALM.py
+++ Script automation/infoNAPALM.py
@@ -3,7 +3,7 @@ from napalm_connection import napalm_connection,napalm_close
 
 def sw():
     device=napalm_connection('192.168.10.254','netauto','netauto','netauto','ios')
-    #print(device.get_facts())
+    print(device.get_facts()['interface_list'])
     #
     #print(device.get_mac_address_table())
     #print(device.get_arp_table())
@@ -14,4 +14,4 @@ def sw():
     napalm_close(device)
 
 
-#sw()
+sw()
--- Script automation/trunk_access.py
+++ Script automation/trunk_access.py
@@ -1,22 +1,67 @@
-import netmiko 
-from netmiko   import ConnectHandler
+import netmiko
+from netmiko import ConnectHandler
+from napalm_connection import napalm_connection, napalm_close
+import json
+
+
 def trunk_access():
-    device_info = {
+    device = napalm_connection('192.168.10.254', 'netauto', 'netauto', 'netauto', 'ios')
+    interface_list = device.get_facts()['interface_list']
+    napalm_close(device)
+
+    devices = {
         'device_type': 'cisco_ios',
         'host': '192.168.10.254',
         'username': 'netauto',
         'password': 'netauto',
         'secret': 'netauto',
-    
-
     }
-    connection = netmiko.ConnectHandler(**device_info)
-    connection.enable()
+    net_connect = ConnectHandler(**devices)
+    
+    trunk_interfaces = []
+    access_interfaces = []
+    
+    for interface_name in interface_list:
+        if "Vlan" in interface_name:
+            continue
+        
+        interface_status = net_connect.send_command(f'show running-config interface {interface_name}')
+        
+        # Check if "switchport mode trunk" is present in the output
+        if "switchport mode trunk" in interface_status:
+            trunk_interfaces.append(interface_name)
+        # Check if "switchport mode access" is present in the output
+        else:
+            access_interfaces.append(interface_name)
+    
+    net_connect.disconnect()
 
-    with ConnectHandler (**device_info) as connection:
-        connection.enable()
-        output = connection.send_command('show interfaces status include connected|notconnect|trunk|access')
-        print(output)
-        connection.disconnect()
+    return trunk_interfaces, access_interfaces
 
-trunk_access()
+
+
+def normalize_interface_name(interface_name):
+    if interface_name.startswith('Ethernet'):
+        return 'Et' + interface_name[8:]
+    return interface_name
+
+def end_user():
+    device = napalm_connection('192.168.10.254', 'netauto', 'netauto', 'netauto', 'ios')
+    mac_table = device.get_mac_address_table()
+    _, access_interfaces = trunk_access()  # Obtenir uniquement les interfaces en mode access
+    
+    normalized_access_interfaces = [normalize_interface_name(interface) for interface in access_interfaces]
+    
+    access_mac_addresses = []
+    for entry in mac_table:
+        normalized_interface = normalize_interface_name(entry['interface'])
+        if normalized_interface in normalized_access_interfaces:
+            access_mac_addresses.append(entry)
+    
+    print(access_mac_addresses)
+
+end_user()
+
+
+
+
